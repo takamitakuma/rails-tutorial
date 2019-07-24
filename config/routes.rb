8b84286c86ba25cda22ca5afcbadd54b62d5363a
高見拓磨 <ksuper_stpreaaa@yahoo.co.jp>
2019-07-24 20:34:28 +0900

    Add user following

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,10 @@
 Rails.application.routes.draw do
   get 'password_resets/new'
-
   get 'password_resets/edit'
-
   get 'password_rsets/new'
-
   get 'password_rsets/edit'
 
   get 'sessions/new'
-
   get 'users/new'
 
   root 'static_pages#home'
@@ -20,9 +16,14 @@ Rails.application.routes.draw do
   get '/login', to: 'sessions#new'
   post '/login', to: 'sessions#create'
   delete '/logout', to: 'sessions#destroy'
-  resources :users
+  resources :users do
+    member do
+      get :following, :followers
+    end
+  end
   resources :account_activations, only: [:edit]
   resources :password_resets,     only: [:new, :create, :edit, :update]
   resources :microposts,          only: [:create, :destroy]
+  resources :relationships,       only: [:create, :destroy]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
--- test/integration/microposts_interface_test.rb
+++ test/integration/microposts_interface_test.rb
@@ -45,4 +45,11 @@ class MicropostsInterfaceTest < ActionDispatch::IntegrationTest
     get root_path
     assert_match "1 micropost", response.body
   end
+
+  test "count relationships" do
+    log_in_as(@user)
+    get root_path
+    assert_match @user.active_relationships.count.to_s, response.body
+    assert_match @user.passive_relationships.count.to_s, response.body
+  end
 end
